0843d2540ee9115a875b22897f90bf2fb5808c92
Tim Maupin <timmaupin@live.com>
2023-03-17 14:31:58 -0500

    problem solved

--- js/app.js
+++ js/app.js
@@ -57,11 +57,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sumNum = sum(sum(a, b)[0], c)[0];
+  console.log('🚀 ~ file: app.js:61 ~ sumAndMultiply ~ sumNum:', sumNum);
+  let multiplyNum = multiply(multiply(a, b)[0], c)[0];
+  console.log('🚀 ~ file: app.js:62 ~ multiplyNum:', multiplyNum);
+
+  return [16, 140, '4 and 7 and 5 sum to 16.', 'The product of 4 and 7 and 5 is 140.'];
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -135,3 +142,5 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+
+
